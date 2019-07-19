d2ce5d7ce791f8c165f0cf6d8465089c5cd5c82c
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-19 10:55:28 +0700

    category hover and bootstrap needed function

--- src/Category.js
+++ src/Category.js
@@ -9,12 +9,19 @@ class Category extends Component {
 	componentWillMount() {
 		console.log(this.props)
 	}
+	getClass = () => {
+		let cl = "badge badge-dark ";
+		if (this.state.category === 'active') {
+			cl += 'active'
+		}
+		return cl
+	}
 
 	// Render
 	render() {
 		return (
 			<div	id="leftcol">
-			<li className={ this.state.category.active ? 'active' : '' } onClick={() => this.props.selectCategory(this.state.category._id)}>#{this.state.category.name}</li>
+			<li className={  this.getClass() } onClick={() => this.props.selectCategory(this.state.category._id)}>{this.state.category.name}</li>
 			</div>
 		)
 	}
